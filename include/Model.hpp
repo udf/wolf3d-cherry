ed84226b7724ff694a62ce404c33f5293f8f53f8
udf <tabhooked@gmail.com>
2019-09-11 14:38:43 +0200

    Add simple player to model

--- include/Model.hpp
+++ include/Model.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
+#include "Point.hpp"
+
 struct Model {
+    // TODO: check if fixed point is faster on pi
+    using Coord = Point<float>;
+
+    Coord player_pos = Coord(0, 0);
+
   private:
     Model(const Model &other) = delete;
     Model &operator=(const Model &other) = delete;
---
+++ include/Point.hpp
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhoosen <mhoosen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 13:14:19 by mhoosen           #+#    #+#             */
+/*   Updated: 2019/06/10 18:46:45 by mhoosen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+// Generic x,y pair
+template<typename T>
+class Point {
+  public:
+    Point() {
+    }
+    Point(T x, T y) : x(x), y(y) {
+    }
+    Point(const Point &other) {
+        *this = other;
+    }
+    Point &operator=(const Point &other) {
+        this->x = other.x;
+        this->y = other.y;
+        return *this;
+    }
+    ~Point() {
+    }
+
+    Point(T v) : x(v), y(v) {
+    }
+
+    bool operator==(const Point &other) const {
+        return this->x == other.x && this->y == other.y;
+    }
+
+    Point operator+(const Point &other) const {
+        return Point(this->x + other.x, this->y + other.y);
+    }
+    Point operator+(const T &val) const {
+        return Point(this->x + val, this->y + val);
+    }
+
+    Point operator-(const Point &other) const {
+        return Point(this->x - other.x, this->y - other.y);
+    }
+    Point operator-(const T &val) const {
+        return Point(this->x - val, this->y - val);
+    }
+
+    Point &operator+=(const Point &other) {
+        this->x += other.x;
+        this->y += other.y;
+        return *this;
+    }
+    Point &operator+=(const T &val) {
+        this->x += val;
+        this->y += val;
+        return *this;
+    }
+
+    Point &operator-=(const Point &other) {
+        this->x -= other.x;
+        this->y -= other.y;
+        return *this;
+    }
+    Point &operator-=(const T &val) {
+        this->x -= val;
+        this->y -= val;
+        return *this;
+    }
+
+    Point operator*(const T &val) const {
+        return Point(this->x * val, this->y * val);
+    }
+    Point operator/(const T &val) const {
+        return Point(this->x / val, this->y / val);
+    }
+
+    T x, y;
+
+    using type = T;
+};
