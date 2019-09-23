26fdf6afc9ff8895a765226c8e85e7949f90325e
udf <tabhooked@gmail.com>
2019-09-23 23:06:53 +0200

    add range map function

--- include/util.hpp
+++ include/util.hpp
@@ -7,3 +7,4 @@ const float pi = static_cast<float>(std::atan(1) * 4);
 float radians(float a);
 float sin_d(float a);
 float cos_d(float a);
+float fmapf(float n, float s1, float e1, float s2, float e2);
--- srcs/util.cpp
+++ srcs/util.cpp
@@ -11,3 +11,7 @@ float sin_d(float a) {
 float cos_d(float a) {
     return std::cos(radians(a));
 }
+
+float fmapf(float n, float s1, float e1, float s2, float e2) {
+	return (s2 + (e2 - s2) * ((n - s1) / (e1 - s1)));
+}
