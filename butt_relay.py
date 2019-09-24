466c1cfeafe94cb5e98c95ea846d30b28094f2f0
Isard <ibotha@student.wethinkcode.co.za>
2019-09-24 19:31:17 +0200

    Add button relay

---
+++ butt_relay.py
@@ -0,0 +1,39 @@
+from gpiozero import Button
+from signal import pause
+from time import sleep
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+pins = {
+        'A': {"pin": 2, "key": 'z'},
+        'B': {"pin": 4, "key": 'x'},
+        'Start': {"pin": 3, "key": 'c'},
+        'R': {"pin": 6, "key": Key.right},
+        'L': {"pin": 0, "key": Key.left},
+        'U': {"pin": 13, "key": Key.up},
+        'D': {"pin": 5, "key": Key.down}
+}
+
+buttons = {}
+
+for key, val in pins.items():
+    b = Button(val["pin"])
+    buttons[key] = {}
+    buttons[key]["button"] = b
+    buttons[key]["value"] = False
+    buttons[key]["key"] = val["key"] 
+
+while True:
+    for name, content in buttons.items():
+        if content["button"].is_pressed == True:
+            if content["value"] == False:
+                content["value"] = True
+                keyboard.press(content["key"])
+        else:
+            if content["value"] == True:
+                content["value"] = False
+                keyboard.release(content["key"])
+
+        
+    sleep(0.05)
