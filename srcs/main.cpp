70aeee408d54f965a50a0183f0d924116c97d4fc
udf <tabhooked@gmail.com>
2019-09-24 20:30:08 +0200

    shaun you fucking retard

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -5,7 +5,7 @@
 int main(int ac, char **av) {
     try {
         Controller controller;
-	ac == 2 ? controller.model.load_map(av[1]) : controller.model.load_map("test.map");
+        controller.model.load_map(ac == 2 ? av[1] : "test.map");
         controller.run();
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
