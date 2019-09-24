393a2fa27923a118ec632d297c469e70e163d61c
udf <tabhooked@gmail.com>
2019-09-24 20:14:26 +0200

    remove more debug

--- srcs/View.cpp
+++ srcs/View.cpp
@@ -279,9 +279,6 @@ void View::draw(const Model &m) {
     for (uint32_t x = 0; x < width; x++) {
         float camX = fmapf((float)(x + 1), 1, (float)width, 1.f, -1.f);
         const Model::Coord ray_dir = m.player.rot_vec + m.cam_rot_vec * camX;
-        if (m.debug) {
-            std::cout << camX << std::endl;
-        }
         auto hit = cast_ray(m, ray_dir);
         if (!hit.tex)
             continue;
