0a4eed4735328b59dd0ded816113d939ef503b46
udf <tabhooked@gmail.com>
2019-09-24 21:03:11 +0200

    Add comp_euc_dist

--- include/util.hpp
+++ include/util.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include "Point.hpp"
 
 const float pi = static_cast<float>(std::atan(1) * 4);
 
@@ -8,3 +9,6 @@ float radians(float a);
 float sin_d(float a);
 float cos_d(float a);
 float fmapf(float n, float s1, float e1, float s2, float e2);
+
+// Euclidian distance for comparisons (without sqrt)
+float comp_euc_dist(Point<float> p1, Point<float> p2);
--- srcs/util.cpp
+++ srcs/util.cpp
@@ -15,3 +15,7 @@ float cos_d(float a) {
 float fmapf(float n, float s1, float e1, float s2, float e2) {
 	return (s2 + (e2 - s2) * ((n - s1) / (e1 - s1)));
 }
+
+float comp_euc_dist(Point<float> p1, Point<float> p2) {
+    return std::sqrt(p1.x - p2.x) + std::sqrt(p1.y - p2.y);
+}
