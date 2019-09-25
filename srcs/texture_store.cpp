cfa5477deafe9484c7cd0b20e2bbb1aa2ed8f9a5
Danie LINDE <dlinde@c5r1s3.wethinkcode.co.za>
2019-09-25 22:27:01 +0200

    Hey

--- srcs/texture_store.cpp
+++ srcs/texture_store.cpp
@@ -43,8 +43,7 @@ const decltype(TextureStore::filename_mapping) TextureStore::filename_mapping{
     {"L2", "tiles/table_inside.png"},
     {"T1", "tiles/table_top.png"},
     {"Z1", "ceilings/void.png"},
-    {"Z2", "floors/void.png"},
-	{"S1", "Sprites/Read_BB.png"}
+    {"Z2", "floors/void.png"}
 };
 
 static Pixel surface_get_pixel(SDL_Surface *surface, size_t i) {
