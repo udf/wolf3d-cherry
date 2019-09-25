362e17fe5c902be1e51c3a80583d42df4e3cbdf0
udf <tabhooked@gmail.com>
2019-09-25 11:00:27 +0200

    update res

--- include/View.hpp
+++ include/View.hpp
@@ -23,8 +23,8 @@ class View {
     void draw_text(const char *text, int x, int y);
     void draw_overlay(const Model &model);
 
-    uint32_t width = 640;
-    uint32_t height = 480;
+    const uint32_t width = 1280;
+    const uint32_t height = 840;
 
     SDL_Window *window = nullptr;
     SDL_Renderer *renderer = nullptr;
